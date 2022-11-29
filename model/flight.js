9dc46b73ba28b53368a7038ae114c18a2101d868
mmorganj41 <mmorganj41@gmail.com>
2022-11-29 15:39:54 -0500

    create flight schema and model

---
+++ model/flight.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const flightSchema = new mongoose.Schema({
+    airline: {
+        type: String,
+        enum: ['American', 'Delta', 'Southwest', 'United'],
+    },
+    airport: {
+        type: String,
+        enum: ['ATL', 'DFW', 'DEN', 'LAX', 'SAN'],
+        default: 'DEN',
+    },
+    flightNo: {
+        type: Number,
+        min: 10,
+        max: 9999,
+    },
+    departs: {
+        type: Date,
+        default: function() {
+            let date = new Date();
+            date.setFullYear(date.getFullYear() + 1);
+            return date;
+        },
+    }
+})
+
+module.exports = mongoose.model('Flight', flightSchema);
