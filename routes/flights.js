8f9d46d61da07a5fc9dd53f74ea4f7981fd7334b
mmorganj41 <mmorganj41@gmail.com>
2022-11-29 15:29:26 -0500

    Create new.ejs

--- routes/flights.js
+++ routes/flights.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const flightsController = require('../controller/flights');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var flightsRouter = require('./routes/flights');
+const indexRouter = require('./routes/index');
+const flightsRouter = require('./routes/flights');
 
 // connect to the database with Mongoose
 require('./config/database');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
