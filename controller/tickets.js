9a58fb6030f8a554c82ec0269500d5ccb378e6ff
mmorganj41 <mmorganj41@gmail.com>
2022-12-01 14:12:07 -0500

    Create route for tickets

--- controller/tickets.js
+++ controller/tickets.js
@@ -1,9 +1,15 @@
 const Ticket = require('../model/ticket');
 
 function newTicket(req, res, next) {
-    res.send('new Ticket works');
+    
+    res.render('tickets/new', {flight: req.params.id});
+}
+
+function create(req, res, next) {
+    res.redirect(`/flights/${req.params.id}`);
 }
 
 module.exports = {
     new: newTicket,
+    create,
 }
--- routes/tickets.js
+++ routes/tickets.js
@@ -3,5 +3,6 @@ const Router = express.Router();
 const ticketsController = require('../controller/tickets');
 
 Router.get('/flights/:id/tickets/new', ticketsController.new);
+Router.post('/flights/:id/tickets', ticketsController.create);
 
 module.exports = Router;
