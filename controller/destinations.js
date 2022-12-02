b6cd0981eb9136429ad943c1dcefaaed6056077e
mmorganj41 <mmorganj41@gmail.com>
2022-12-02 11:59:43 -0500

    destinations refactor async and await

--- controller/destinations.js
+++ controller/destinations.js
@@ -1,50 +1,29 @@
 const Flight = require('../model/flight');
 
-function create(req, res, next) {
-    Flight.findById(req.params.id, (err, flight) => {
-        if (err) {
-            console.log(err);
-            return res.send('Error finding by ID check terminal');
-        }
-
-        console.log(flight);
-        
+async function create(req, res, next) {
+	try {
+		const flight = await Flight.findById(req.params.id);
+        console.log(flight);   
         flight.destinations.push(req.body);
-
-        flight.save(err => {
-            if(err){
-                console.log(err);
-                return res.send('Error saving to database');
-            }
-
-            res.redirect(`/flights/${req.params.id}`);
-        });
-    })
-    
+		await flight.save();
+		res.redirect(`/flights/${req.params.id}`);
+	} catch(err) {
+		console.log(err);
+		return res.send('Error finding by ID check terminal');
+	}    
 }
 
-function deleteOne(req, res, next) {
-
-    Flight.findOne({'destinations._id': req.params.id}, (err, flight) => {
-        if (err) {
-            console.log(err);
-            return res.send('Error finding destination to delete');
-        }
-
-        flight.destinations.remove(req.params.id);
-
-        console.log(flight);
-
-        flight.save(err => {
-            if (err) {
-                console.log(err);
-                return res.send('Error saving flight to database')
-            }
-            res.redirect(`/flights/${flight._id}`);
-        })
-
-        
-    });
+async function deleteOne(req, res, next) {
+	try {
+		const flight = await Flight.findOne({'destinations._id': req.params.id});
+		flight.destinations.remove(req.params.id);
+		console.log(flight);
+		await flight.save();
+		res.redirect(`/flights/${flight._id}`);
+	} catch(err) {
+		console.log(err);
+		return res.send('Error finding destination to delete');
+	}
 }
 
 module.exports = {
